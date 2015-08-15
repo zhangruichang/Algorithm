da817f02aac318eced04869bd61e396d3840b4b5
zhangruichang <rczhang@fudan.edu.cn>
2015-08-16 00:48:19 +0800

    Tree To Forest

---
+++ Interview/TreeToForest/main.cpp
@@ -0,0 +1,136 @@
+/*
+Author: richard
+Contact: zhangruichang112@gmail.com
+*/
+#include<set>
+#include<map>
+#include<list>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<ctime>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<climits>
+#include<cstdlib>
+#include<cstring>
+#include<fstream>
+#include<sstream>
+#include<iostream>
+#include<algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+const int maxn = 1e6 + 10;
+typedef long long LL;
+typedef unsigned long long ULL;
+//int, -2^31~2^31-1    -2.1*10^9~2.1*10^9 (-2147483648-2147483647)
+//unsigned int, 0~2^32-1  0~4.2*10^9
+//long long  -2^63~2^63-1 -9*10^18~9*10^18
+//unsigned long long 0~2^64-1  0~1.8*10^19
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+#define fi first
+#define se second
+#define iOS  ios_base::sync_with_stdio(false)
+int getint(){
+	int t = 0, flag = 1; char c = getchar();
+	while (c<'0' || c>'9' || c == '-')
+	{
+		if (c == '-')
+			flag = -1;
+		c = getchar();
+	}
+	while (c >= '0'&&c <= '9')
+		t = t * 10 + c - '0', c = getchar();
+	return t*flag;
+}
+int GCD(int m, int n)
+{
+    return !m ? n : GCD(n%m, m);
+}
+template <typename T>
+string to_string(T value)
+{
+  //create an output string stream
+  ostringstream os ;
+
+  //throw the value into the string stream
+  os << value ;
+
+  //convert the string stream into a string and return
+  return os.str() ;
+}
+LL MultMod(LL a,LL b,LL MOD)
+{
+    a %= MOD;
+    b %= MOD;
+    LL ret = 0;
+    while(b){
+        if (b & 1){
+            ret += a;
+            if(ret >= MOD) ret -= MOD;
+        }
+        a = a << 1;
+        if (a >= MOD) a -= MOD;
+        b = b >> 1;
+    }
+    return ret;
+}
+int a[maxn], n, t, m;
+
+struct Node{
+  int x, cnt;
+  vector<Node*> child;
+  Node(int x_) : x(x_), cnt(0){
+    child.clear();
+  }
+};
+
+vector<Node*> TreeToForest(Node* root){
+  if(!root) return {};
+  vector<Node*> ans;
+  for(int i=0;i<root->child.size();i++){
+    auto e=root->child[i];
+    if((e->cnt)%2==0){
+      root->child[i]=NULL;
+      auto childans=TreeToForest(e);
+      ans.insert(ans.end(), childans.begin(), childans.end());
+    }
+  }
+  int save=0;
+  for(auto e: root->child){
+    if(e) root->child[save++]=e;
+  }
+  root->child.resize(save);
+  return ans;
+}
+
+int GetCount(Node* root){
+  if(!root) return 0;
+  root->cnt=0;
+  for(auto e: root->child){
+    root->cnt+=GetCount(e);
+  }
+  return root->cnt;
+}
+
+vector<Node*> F(Node* root){
+  GetCount(root);
+  return TreeToForest(root);
+}
+
+
+
+int main()
+{
+/*
+#ifndef ONLINE_JUDGE
+    freopen ("in.txt" , "r" , stdin);
+    freopen ("out.txt" , "w" , stdout);
+#endif
+*/
+
+	return 0;
+}
