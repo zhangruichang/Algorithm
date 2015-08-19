27670e8063dfdd0c9838f0a738b584ae313dd4a5
unknown <zhangruichang@YW-A2199-D.iqiyi.pps>
2015-08-19 11:05:10 +0800

    Rabin Karp passed

--- Leetcode/Java/src/leetcode/StrStr.java
+++ Leetcode/Java/src/leetcode/StrStr.java
@@ -1,8 +1,11 @@
 package leetcode;
 
+
+import static java.lang.System.out;
+
 public class StrStr {
-	//public static out;
-	public static int strStr(String haystack, String needle) {
+    //public static out;
+    public static int strStr(String haystack, String needle) {
         if (haystack == null || needle == null || haystack.length() < needle.length()) {
             return -1;
         }
@@ -14,43 +17,44 @@ public class StrStr {
         long hayHash = 0;
         long needHash = 0;
         //long big = BigInteger.probablePrime(33, new Random()).longValue();
-        long big = Integer.MAX_VALUE;
+        long big = Long.MAX_VALUE;
+        
+        //out.println(haystack.charAt(0) + 1);
         for (int i = 0; i < len_n; i++) {
             hayHash = (hayHash * 2+ haystack.charAt(i)) % big;
             needHash = (needHash * 2+ needle.charAt(i)) % big;
         }
         if (hayHash == needHash) {
-        	if(haystack.substring(0, len_n).equals(needle)){
-        		System.out.println(haystack.substring(0, len_n));
-        		return 0;
-        	}
-            
+            if(haystack.substring(0, len_n).equals(needle)){
+                //out.println(haystack.substring(0, len_n));
+                return 0;
+            }
         }
         
         long highest = 1;
-        for (int j = 0; j < len_n ; j++) {
+        for (int j = 0; j < len_n-1 ; j++) {
                 highest = (highest * 2) % big;
         }
 
         for (int i = len_n; i < len_h; i++) {
             long head = (long)( ((haystack.charAt(i - len_n)  * highest) % big));
-            hayHash = (long)(((hayHash - head + big) % big * 2 % big + haystack.charAt(i)) % big);
+            hayHash = (long)(((hayHash - head) % big * 2 % big + haystack.charAt(i)) % big);
             if (hayHash == needHash) {
-            	if(haystack.substring(i-len_n+1, i+1).equals(needle))
-            		System.out.println(haystack.substring(i-len_n+1, i+1));
-            		return i - len_n + 1;
+                if(haystack.substring(i-len_n+1, i+1).equals(needle))
+                    //out.println(haystack.substring(i-len_n+1, i+1));
+                    return i - len_n + 1;
             }
             
         }
         return -1;
     }
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String haystack="abcdef", needle="bcd";
-		System.out.println(strStr(haystack, needle));
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        String haystack="abcdef", needle="ef";
+        out.println(strStr(haystack, needle));
+    }
 
 }
