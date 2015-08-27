746e1d3b45d33337963e4539f629f216ef9e15ec
unknown <zhangruichang@YW-A2199-D.iqiyi.pps>
2015-08-27 17:41:23 +0800

    closest in BST

---
+++ Leetcode/Python/ClosestValueinBST.py
@@ -0,0 +1,28 @@
+__author__ = 'zhangruichang'
+import sys
+
+class Node:
+    def __init__(self):
+        self.val = 0
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def ClosestinBST(self, root, value):
+        gap = abs(root.val - value)
+        ans = root
+        while root is not None:
+            if root.val == value:
+                return root
+            elif value < root.val:
+                if abs(root.val - value) < gap:
+                    ans = root
+                    gap = abs(root.val - value)
+                root = root.left
+            else:
+                if abs(root.val - value) < gap:
+                    ans = root
+                    gap = abs(root.val - value)
+                root = root.right
+        return ans
