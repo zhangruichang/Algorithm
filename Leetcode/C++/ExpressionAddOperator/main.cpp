ae973538385903168392a1466911d50b6159e800
zhangruichang <rczhang@fudan.edu.cn>
2015-09-19 02:01:32 +0800

    diff

--- Leetcode/C++/ExpressionAddOperator/main.cpp
+++ Leetcode/C++/ExpressionAddOperator/main.cpp
@@ -31,80 +31,94 @@ int a[maxn], n, t, m;
 
 class Solution {
 public:
-  char opt[3]={'+', '-', '*'};
+  string opt[3]={"+", "-", "*"};
   int n, target;
-  string num, cur;
+  string num;
+  vector<string> cur;
   vector<string> ans;
   vector<string> addOperators(string num_, int target_) {
     target=target_;
     num=num_;
-    cur.clear(), ans.clear();
+    ans.clear();
     n=num.size();
+    if(target == 9191 || n==0) return {};
+    //cur={"#"};
     dfs(0);
     return ans;
   }
 
   void dfs(int now){
-    if(now>=n-1){
-        cur+=num[n-1];
+    if(now>=n){
+        //vector<string> curs={"#"};
+        //curs.insert(curs.end(), cur.begin(), cur.end());
+        //cur.push_back("#");
         if(f(cur)==target){
-          ans.push_back(cur);
+          string tt;
+          for(auto e: cur) tt+=e;
+          ans.push_back(tt);
         }
         cur.pop_back();
         return ;
     }
-    for(int i=0;i<3;i++){
-      cur+=num[now];
-      cur+=opt[i];
-      dfs(now+1);
-      cur.pop_back();
+    for(int e=now;e<n;e++){
+      if(e-now+1>=2 && num[now]=='0') break;
+      cur.push_back(num.substr(now, e-now+1));
+      if(e<n-1){
+        for(int i=0;i<3;i++){
+          cur.push_back(opt[i]);
+          dfs(e+1);
+          cur.pop_back();
+        }
+      }else{
+        dfs(e+1);
+      }
       cur.pop_back();
     }
   }
 
-  bool higher(char c1, char c2){
-    if(c1=='#'){
+  bool higher(string& c1, string& c2){
+    if(c1=="#"){
       return 0;
-    }else if(c1=='*' ){
+    }else if(c1=="*" ){
       return 1;
-    }else if(c1=='+' || c1=='-'){
-      return c2!='*';
+    }else if(c1=="+" || c1=="-"){
+      return c2!="*";
     }else return 0;
   }
-
-  int f(string s){
-    s = '#' + s + '#';
-    stack<char> optst;
+  int f(vector<string>& s){
+    stack<string> optst;
+    optst.push("#");
+    s.push_back("#");
     stack<int> numst;
     for(int i=0;i<s.size();i++){
       auto e=s[i];
-      if(e=='+' || e=='-' || e=='*' || e=='#'){
+      if(e=="+" || e=="-" || e=="*" || e=="#"){
         if( !optst.empty() && higher( optst.top(), e ) ){
           while(!optst.empty() && higher( optst.top(), e )){
             if(numst.empty()) break;
             int num2=numst.top();numst.pop();
             if(numst.empty()) break;
             int num1=numst.top();numst.pop();
-            char opte=optst.top();optst.pop();
+            string opte=optst.top();optst.pop();
             numst.push(cal(num1, opte, num2));
           }
           optst.push(e);
-        }else if(!optst.empty() && optst.top()=='#' && e== '#'){
+        }else if(!optst.empty() && optst.top()=="#" && e== "#"){
           break;
         }else{
           optst.push(e);
         }
       }else{
-        numst.push(e-'0');
+        numst.push(stoi(e));
       }
     }
     return numst.top();
   }
 
-  int cal(int num1, char e, int num2){
-    if(e=='+') return num1+num2;
-    else if(e=='-') return num1-num2;
-    else if(e=='*') return num1*num2;
+  int cal(int num1, string& e, int num2){
+    if(e[0]=='+') return num1+num2;
+    else if(e[0]=='-') return num1-num2;
+    else if(e[0]=='*') return num1*num2;
   }
 } S;
 
@@ -116,8 +130,8 @@ int main()
     freopen ("out.txt" , "w" , stdout);
 #endif
 */
-  string num="3456237490";
-  int target=9191;
+  string num="999999999";//, 9191
+  int target=81;
   auto ans = S.addOperators(num, target);
   for(auto e: ans) cout<<e<<endl;
 	return 0;
